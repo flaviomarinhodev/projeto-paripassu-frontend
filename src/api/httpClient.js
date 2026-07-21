d4b283d3fee1409bd6c085b27b1be3b39c110cf8
Flávio Marinho <flaviomarinho.tec@gmail.com>
2026-07-21 17:16:27 -0300

    Add route render

--- src/api/httpClient.js
+++ src/api/httpClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL || 'http://localhost:8081/api',
+  //baseURL: process.env.VUE_APP_API_BASE_URL || 'http://localhost:8081/api',
+  baseURL: 'https://projeto-paripassu-backend.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
